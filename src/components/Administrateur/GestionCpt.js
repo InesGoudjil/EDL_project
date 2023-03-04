926a66d8b34c79a8e3de2cb80fecb7ed1b5eafee
InesGoudjil <goudjilines25@gmail.com>
2023-03-03 18:00:14 -0930

    working more on the thing

--- src/components/Administrateur/GestionCpt.js
+++ src/components/Administrateur/GestionCpt.js
@@ -1,13 +1,49 @@
 import React from "react";
-import './GestionCpt.css';
+import "./GestionCpt.css";
 import Header from "../Header";
+import SideBar from "../SideBar";
 
-function GestionCpt (){
-    return(
-        <div className="gestion_all">
-         <Header></Header>
+function GestionCpt() {
+  return (
+    <div className="gestion_all">
+      <Header></Header>
+      {/* <SideBar></SideBar> */}
+      <div className="container___">
+      {/* <div className="title">
+        <h2>Gestion des comptes:</h2>
+      </div> */}
+        <div className="ajout">
+          <div className="ajout_sec">
+            <h3>Ajout des comptes</h3>
+          </div>
+          <div>
+          <select className="ajout_sec">
+            <option value="">Pour Enseignant</option>
+            <option value="">Pour Condidat</option>
+            <option value="">Pour Vice-doyen</option>
+            <option value="">Pour Président CFD</option>
+            <option value="">Pour Administrateur</option>
+          </select>
+          </div>
+          
         </div>
-    )
+        <div className="suppression">
+          <div className="suppression_sec">
+            <h3>Suppression des comptes</h3>
+          </div>
+          <div>
+          <select className="suppression_sec">
+            <option value="">Pour Enseignant</option>
+            <option value="">Pour Condidat</option>
+            <option value="">Pour Vice-doyen</option>
+            <option value="">Pour Président CFD</option>
+            <option value="">Pour Administrateur</option>
+          </select>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default GestionCpt;
